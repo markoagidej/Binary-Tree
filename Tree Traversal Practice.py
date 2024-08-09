d3f5eac0a94a1c7068be01bf336057191e716cd1
Marko <markogidej@yahoo.com>
2024-08-08 21:05:32 -0400

    Init Commit

---
+++ Tree Traversal Practice.py
@@ -0,0 +1,22 @@
+'''
+                50
+           ┌─────┴─────┐
+          30           70
+     ┌────┴────┐    ┌────┴────┐
+    40       20   60         80
+'''
+
+# Task 1 Implement the in-order traversal algorithm for the given binary tree.
+
+
+
+# Task 2 Implement the pre-order traversal algorithm for the given binary tree.
+
+
+
+# Task 3 Implement the post-order traversal algorithm for the given binary tree.
+
+
+
+# task 4 Test the implemented traversal algorithms on a binary tree and observe the traversal order and output.
+
